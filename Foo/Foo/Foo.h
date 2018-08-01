c8fbc033f15fca01579c7e89a040bceee63cbba2
wtao <wtao@mux.com>
2018-08-01 10:17:20 -0700

    Init dummy lib

---
+++ Foo/Foo/Foo.h
@@ -0,0 +1,19 @@
+//
+//  Foo.h
+//  Foo
+//
+//  Created by wtao on 8/1/18.
+//  Copyright © 2018 example. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Foo.
+FOUNDATION_EXPORT double FooVersionNumber;
+
+//! Project version string for Foo.
+FOUNDATION_EXPORT const unsigned char FooVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Foo/PublicHeader.h>
+
+
---
+++ Foo/Foo/Foo.swift
@@ -0,0 +1,15 @@
+//
+//  Foo.swift
+//  Foo
+//
+//  Created by wtao on 8/1/18.
+//  Copyright © 2018 example. All rights reserved.
+//
+
+import Foundation
+
+class Foo {
+    func add(_ a: Int, _ b: Int) -> Int {
+        return a + b;
+    }
+}
